548dadeeb1bdad75042fdbd28f898c59c2342bdb
Emmanuel <blaemedia26@gmail.com>
2026-01-09 18:18:05 +0100

    Implementing Mobile Components in React Native

---
+++ prodev-mobile-app-0x01/app/index.tsx
@@ -0,0 +1,46 @@
+import { Text, View, StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: "#90caf9",
+    padding: 20,
+    flex: 1,
+  },
+  largeText: {
+    fontSize: 30,
+    color: "#f44336",
+    marginBottom: 5,
+    fontWeight: "700",
+    fontVariant: ["small-caps"],
+  },
+  mediumText: {
+    fontSize: 20,
+    color: "#9c27b0",
+    marginBottom: 10,
+    fontWeight: "500",
+    textAlign: "right",
+  },
+  smallText: {
+    fontSize: 15,
+    color: "#2196f3",
+    fontWeight: "400",
+    textAlign: "center",
+  },
+});
+
+export default function Index() {
+  return (
+    <View style={styles.container}>
+      <Text>Entry Screen - Awesome</Text>
+      <Text style={styles.largeText}>
+        TypeScript is great if you practice more
+      </Text>
+      <Text style={styles.mediumText}>
+        React Native provides you a single codebase for cross platforms
+      </Text>
+      <Text style={styles.smallText}>
+        ALX is awesome
+      </Text>
+    </View>
+  );
+}
